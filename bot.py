98895cd55a27af3a5e23035b7b048691b57fb105
Elias-YJ <elias.yla-jarkko@live.fi>
2019-10-22 21:36:02 +0300

    Create functional bot skeleton
    Bot checks if user's id is on the permissions list or not.

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
-print("Hello world")
+import util
+import telegram.ext
+
+
+def start(update, context):
+    user: telegram.User = update.message.from_user
+    if util.is_user_permitted(user.id):
+        update.message.reply_text("You have editing permissions.")
+    else:
+        update.message.reply_text("You do not have editing permission.")
+
+
+def main():
+    updater = telegram.ext.Updater(util.get_bot_token(), use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(telegram.ext.CommandHandler("start", start))
+
+    updater.start_polling()
+    # updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ util.py
@@ -0,0 +1,13 @@
+def get_bot_token():
+    text = open('token.txt', 'r')
+    token = text.readlines()[0]
+    text.close()
+    return token
+
+
+def is_user_permitted(user_id: int):
+    with open('permissions.txt', 'r') as permissions:
+        if str(user_id) in permissions.read():
+            return True
+        else:
+            return False
