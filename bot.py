9f97080fb1866203b3b4f9a21123b582aece346f
Elias-YJ <elias.yla-jarkko@live.fi>
2019-10-27 17:32:46 +0200

    Bot interacts with the database
    Stickers are now being added to the database and they can be queued with
    inline queries. Adding and removing keywords has been somewhat tested,
    but reliability is not guaranteed. Inline queries execute with SQL LIKE
    syntax, which makes incomplete queries still match stickers.

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import util
+import os
 import logging
 import telegram.ext
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import (ConversationHandler, CommandHandler, MessageHandler, Filters)
+from telegram import ReplyKeyboardMarkup, InlineQueryResultCachedSticker, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import (ConversationHandler, CommandHandler, MessageHandler,
+                          InlineQueryHandler, Filters)
 from database_tools import DatabaseEditor
+from uuid import uuid4
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,7 +18,9 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 CHOOSING, SELECTING_STICKER, SELECTING_KEYWORD = range(3)
 
-db_editor = DatabaseEditor()
+parent_dir: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+database = os.path.join(parent_dir, 'Stiguhakubot_files', 'stickers.db')
+db_editor = DatabaseEditor(database)
 
 
 def start(update, context):
@@ -39,6 +44,16 @@ def edit(update, context):
         update.message.reply_text("You do not have editing permission.")
 
 
+def info(update, context):
+    update.message.reply_text("This bot is used in inline mode. "
+                              "Type \"@stiguhakubot\" followed by your search term to search for stickers.")
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logging.warning('Update "%s" caused error "%s"', update, context.error)
+
+
 def add_keyword(update, context):
     update.message.reply_text("Adding a keyword. Send me a sticker that will be searched for.")
     context.user_data["add"] = True
@@ -55,12 +70,13 @@ def select_sticker(update, context):
     sticker: str = update.message.sticker.file_id
     context.user_data["current_sticker"] = sticker
     if context.user_data.get("add", True):
-        update.message.reply_text("Sticker with ID:{} selected. Now type a keyword for the sticker".format(
-                                sticker))
+        update.message.reply_text("Sticker with ID:{} selected. Now type a keyword for the sticker. "
+                                  "The keyword will be converted to lower case".format(sticker))
     else:
+        result = db_editor.get_keywords(sticker)
         update.message.reply_text("Sticker with ID:{} selected. Now type a keyword you would like to remove. "
-                                  "The sticker currently has these keywords: "
-                                  "{} ".format(sticker, db_editor.get_keywords(sticker)))
+                                  "The keyword will be converted to lower case. {}"
+                                  .format(sticker, result))
     return SELECTING_KEYWORD
 
 
@@ -70,15 +86,25 @@ def select_keyword(update, context):
     logging.info(update.message.from_user.full_name + " performed an edit operation")
     update.message.reply_text("Selected keyword is \"{}\". {}"
                               "Choose next action or \"Done\" "
-                              .format(update.message.text, result), reply_markup=markup)
+                              .format(update.message.text.lower(), result), reply_markup=markup)
     return CHOOSING
 
 
 def done(update, context):
     context.user_data["current_sticker"] = ""
     context.user_data["current_keyword"] = ""
-    update.message.reply_text("Interaction ended succesfully")
+    no_keyboard = telegram.ReplyKeyboardRemove()
+    update.message.reply_text("Interaction ended succesfully", reply_markup=no_keyboard)
     return ConversationHandler.END
+
+
+def inlinequery(update, context):
+    query: str = update.inline_query.query
+    # Results will be returned from the database insead of a hard coded answer.
+    raw_results = db_editor.search_stickers(query)
+    results = list(map(lambda x: InlineQueryResultCachedSticker(id=uuid4(), sticker_file_id=x), raw_results))
+
+    update.inline_query.answer(results)
 
 
 def main():
@@ -91,14 +117,20 @@ def main():
         states={
             CHOOSING: [MessageHandler(Filters.regex('^(Add keyword)$'), add_keyword),
                        MessageHandler(Filters.regex('^(Remove keyword)$'), remove_keyword)],
-            SELECTING_STICKER: [MessageHandler(Filters.sticker, select_sticker)],
-            SELECTING_KEYWORD: [MessageHandler(Filters.text, select_keyword)]
+            SELECTING_STICKER: [MessageHandler(Filters.regex('^Done$'), done),
+                                MessageHandler(Filters.sticker, select_sticker)],
+            SELECTING_KEYWORD: [MessageHandler(Filters.regex('^Done$'), done),
+                                MessageHandler(Filters.text, select_keyword)]
         },
         fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
     )
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", info))
     dp.add_handler(conv_handler)
+    dp.add_handler(InlineQueryHandler(inlinequery))
+
+    dp.add_error_handler(error)
 
     updater.start_polling()
     # updater.idle()
--- database_tools.py
+++ database_tools.py
@@ -1,18 +1,70 @@
+import sqlite3
+
+
 class DatabaseEditor:
-    def __init__(self):
-        pass
+    def __init__(self, database):
+        self.database = database
 
     # INSERT a new row to the database
     def add_keyword(self, sticker_id, keyword):
+        data = (sticker_id, keyword.lower())
+        try:
+            conn = sqlite3.connect(self.database)
+            c = conn.cursor()
+            c.execute('''INSERT INTO Stickers
+                    VALUES (?,?);''', data)
+            conn.commit()
+            conn.close()
+        except sqlite3.IntegrityError:
+            conn.close()
+            return "The sticker already has that keyword. Please choose another sticker or keyword."
+        except sqlite3.DatabaseError:
+            return "Connection to the database is not availble. Please try again later."
         return "Keyword added succesfully. "
 
     # Get (SELECT) keys from the database for a sticker
     def get_keywords(self, sticker_id):
-        pass
+        sticker_id = (sticker_id,)
+        result = []
+        try:
+            conn = sqlite3.connect(self.database)
+            c = conn.cursor()
+            for row in c.execute('''SELECT Keyword FROM Stickers WHERE ID=?;''', sticker_id):
+                result.append(row[0])
+            conn.close()
+        except sqlite3.DatabaseError:
+            return "Connection to the database is not availble. Please try again later."
+        if len(result) == 0:
+            return "The sticker has no keywords registered"
+        else:
+            return "The sticker has these keywords registered: {}".format('\n'.join(result))
 
     # DELETE a row from the database
     def remove_keyword(self, sticker_id, keyword):
+        data = (sticker_id, keyword.lower())
+        try:
+            conn = sqlite3.connect(self.database)
+            c = conn.cursor()
+            c.execute('''DELETE FROM Stickers WHERE ID=? AND Keyword=?;''', data)
+            conn.commit()
+            conn.close()
+        except sqlite3.DatabaseError:
+            return "Connection to the database is not availble. Please try again later."
         return "Keyword removed succesfully. "
+
+    def search_stickers(self, keyword):
+        keyword = ('%'+keyword+'%',)
+        result = []
+        try:
+            conn = sqlite3.connect(self.database)
+            c = conn.cursor()
+            for row in c.execute('''SELECT ID FROM Stickers WHERE Keyword LIKE ?;''', keyword):
+                result.append(row[0])
+            conn.close()
+        except sqlite3.DatabaseError:
+            return result
+
+        return result
 
     # Execute selected action (add or remove)
     def perform_action(self, user_data):
